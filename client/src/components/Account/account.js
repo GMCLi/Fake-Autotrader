1dbf4b2d13f4b1d61a874e2ca4e1f687a9cee057
Gordon Li <li.mc.gordon@gmail.com>
2019-04-27 14:05:20 -0400

    account cards

--- client/src/components/Account/account.js
+++ client/src/components/Account/account.js
@@ -4,7 +4,19 @@ import Signup from "../SignUp/signUpModal";
 class Account extends Component {
 
   render() {
-    const check = this.props.signedin ? <div>Signed In</div> :
+    const check = this.props.signedin ?
+      <div>
+        <div className="card">
+          Your Favorite Articles
+      </div>
+        <div className="card">
+          Your Favorite Listings
+      </div>
+        <div className="card">
+          Your Favorite Listings
+      </div>
+      </div>
+      :
       <div>
         Please Sign In
         <Signup />
